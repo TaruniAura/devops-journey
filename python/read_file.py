ed67ce4c55e363fb0af7f8e0a27843f02e796a78
TaruniAura <tarunis1357@gmail.com>
2026-08-11 09:03:45 +0000

    file handling

--- python/read_file.py
+++ python/read_file.py
@@ -1,3 +1,5 @@
+"""
+#Read a file
 file = open("sample.txt", "r")
 
 content = file.read()
@@ -5,3 +7,20 @@ content = file.read()
 print(content)
 
 file.close()
+
+#Better approach — with open()
+with open("sample.txt","r") as file:
+    content=file.read()
+
+print(content)   """
+
+#Read line by line
+with open("sample.txt","r") as file:
+    for line in file:
+        print(line.strip())
+
+#readlines()
+with open("sample.txt","r") as file:
+    lines=file.readlines()
+
+print(lines)    
---
+++ python/write_file.py
@@ -0,0 +1,4 @@
+with open("output.txt", "w") as file:
+    file.write("Hello DevOps!\n")
+    file.write("Learning Python.\n")
+    file.write("Working with files.\n")
